b9f5ee9ed7ce6e80ca73c0492f6cc6a82583e720
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-27 17:46:40 +0100

    Working on the To-do list

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import Header from './components/Header';
 import Todo from './components/To-do';
 import Done from './components/Done';
 import Goals from './components/Goals';
+// import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  // const [list, setList] = useState([])
+// const listFromLocalStorage = JSON.parse(localStorage.getItem("list"))
+
+
   
   return (
     <div className="App">
--- src/components/To-do.js
+++ src/components/To-do.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function Todo(props) {
+
+    const [inputContents, setInputContents] = useState('')
+    let listArr = []
+    const handler = (event) => {
+        setInputContents(event.target.value)
+    }
+    function handleClick(){
+        listArr.push(inputContents)
+        console.log(listArr)
+    }
+
+
     return (
         <div>
             <h2>Things that I need to do still!</h2>
             <ul>
-                <li>Holder 1</li>
-                <li>Holder 2</li>
-                <li>Holder 3</li>
+
             </ul>
+            
+            <input id="goals-input"
+             type="text"
+             placeholder='Add an item here'
+             onChange={handler}
+             />
+             <button onClick={handleClick}>Fuck</button>
+            
         </div>
     );
 }
